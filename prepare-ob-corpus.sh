8770ca58e116be6438a3c4f2a5805276e7c92425
André Lynum <stinky@Macintosh.local>
2010-01-23 17:50:30 +0100

    Added generation of held.source file.

--- prepare-ob-corpus.sh
+++ prepare-ob-corpus.sh
@@ -4,6 +4,7 @@ cor_file=$1
 sent_file=${cor_file}.sent
 train_file=${cor_file}.train
 held_file=${cor_file}.held
+held_source_file=${held_file}.source
 eval_file=${cor_file}.eval
 hunpos_model_file=${cor_file}.hunpos_model
 
@@ -12,6 +13,9 @@ ruby cor-to-train.rb ${cor_file} > ${sent_file}
 
 echo "Splitting corpus into " ${train_file} "," ${held_file} "and" ${eval_file}
 ruby corpus-split.rb ${train_file} ${held_file} ${eval_file} < ${sent_file}
+
+echo "Creating held source file" ${held_source_file}
+awk '{print $1}' ${held_file} > ${held_source_file}
 
 echo "Training HunPos model file" ${hunpos_model_file}
 hunpos-1.0-macosx/hunpos-train ${hunpos_model_file} < ${train_file}
