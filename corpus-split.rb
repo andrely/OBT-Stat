a229c878985d0a8eb5893e87596157aeb256721f
André Lynum <stinky@Macintosh.local>
2010-03-08 23:42:09 +0100

    Added interleave splitting for cor input.

--- corpus-split.rb
+++ corpus-split.rb
@@ -10,6 +10,7 @@ require 'obno_stubs'
 require 'obno_text'
 
 $input_type = nil
+$split_type = :interleave
 
 # interleaved
 def do_normal_input(train_file, held_file, eval_file)
@@ -59,7 +60,7 @@ def print_ob_word(word, file=$stdout)
 end
 
 # block
-def do_cor_input(train_file, held_file, eval_file)
+def do_cor_input_block(train_file, held_file, eval_file)
   text = Text.new
 
   OBNOText.parse text, $stdin.read
@@ -100,8 +101,42 @@ def do_cor_input(train_file, held_file, eval_file)
   end
 end
 
+def do_cor_input_interleave(train_file, held_file, eval_file)
+  text = Text.new
+
+  OBNOText.parse text, $stdin.read
+
+  index = 0
+  fd = nil
+  File.open(train_file, 'w') do |tf|
+    File.open(held_file, 'w') do |hf|
+      File.open(eval_file, 'w') do |ef|
+        text.sentences.each do |s|
+          case index
+            when 0..7:
+              fd = tf
+              index += 1
+            when 8:
+              fd = hf
+              index += 1
+            else
+              fd = ef
+              index = 0
+          end
+
+          s.words.each do |w|
+            print_ob_word(w, fd)
+          end
+        end
+      end
+    end
+  end
+end
+
 if __FILE__ == $0
-  opts = GetoptLong.new(["--type", "-t", GetoptLong::REQUIRED_ARGUMENT])
+  opts = GetoptLong.new(["--type", "-t", GetoptLong::REQUIRED_ARGUMENT],
+                        ["--interleave", "-i", GetoptLong::NO_ARGUMENT],
+                        ["--block", "-b", GetoptLong::NO_ARGUMENT])
 
   opts.each do |opt, arg|
     case opt
@@ -111,15 +146,28 @@ if __FILE__ == $0
         else
           raise RuntimeError
         end
+    when "--interleave":
+        $split_type = :interleave
+    when "--block":
+        $split_type = :block
     end
   end
 
   train_file = ARGV[0]
   held_file = ARGV[1]
   eval_file = ARGV[2]
+
+  if $input_type.nil? and $split_type == :block
+    raise RuntimeError
+  end
   
   if $input_type == :cor
-    do_cor_input(train_file, held_file, eval_file)
+    if $split_type == :interleave
+      do_cor_input_interleave(train_file, held_file, eval_file)
+    else
+      do_cor_input_block(train_file, held_file, eval_file)
+    end
+    
   else
     do_normal_input(train_file, held_file, eval_file)
   end
