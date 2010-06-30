154ca28d056a2e8fef692c797294667112a05ee8
Andre Lynum <and-ly@online.no>
2010-06-30 11:43:46 +0200

    Script for doing 10-fold evaluation run.

---
+++ evaluate.sh
@@ -0,0 +1,7 @@
+for i in 0 1 2 3 4 5 6 7 8 9
+do
+  ruby disamb-proto.rb -l log -v -a data-test/${i}/trening-u-flert-d.cor.lemma_model \
+	-m data-test/${i}/trening-u-flert-d.cor.hunpos_model \
+	-e data-test/${i}/trening-u-flert-d.cor.eval \
+	< data-test/${i}/trening-u-flert-d.cor.eval.source.ob.latin1
+done
